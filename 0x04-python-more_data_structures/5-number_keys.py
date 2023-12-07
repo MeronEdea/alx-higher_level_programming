c347b2eb6b3226ed1b109549655a82d6131356f8
Meron Edea <edeameron@gmail.com>
2023-12-07 10:43:15 -0900

    updating files

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    a = 0
     nb_keys  = list(a_dictionary.keys())
 
     for i in nb_keys :
-        a += 1
+        i += 1
 
-    return (a)
+    return (i)
