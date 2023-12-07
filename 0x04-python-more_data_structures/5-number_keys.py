d27ec57f0e2245fb6a5170d6530a716319fe8acb
Meron Edea <edeameron@gmail.com>
2023-12-07 10:17:15 -0900

    updating files

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
+    a = 0
+    nb_keys  = list(a_dictionary.keys())
+
+    for i in nb_keys :
+        a += 1
+
+    return (a)
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    sorted_dictionary = list(a_dictionary.keys())
+    sorted_dictionary.sort()
+    for i in sorted_dictionary:
+        print("{}: {}".format(i, a_dictionary.get(i)))
