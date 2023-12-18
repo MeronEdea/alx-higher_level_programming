8ec3de07b544fdb9db71a7b0882e18e183a2f1f3
Meron Edea <edeameron@gmail.com>
2023-12-18 08:59:36 -0900

    adding files

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,7 @@ def safe_print_division(a, b):
     try:
         Quotient = a / b
     except ZeroDivisionError:
-        return None
+        Quotient = None
     finally:
         print("Inside result: {}".format(Quotient))
-
-    return Quotient
+        return Quotient
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     Quotient = []
-    try:
-        for i in range(list_length):
-            try:
-                dividend = my_list_1[i]
-                divisor = my_list_2[i]
-                try:
-                    division = dividend / divisor
-                    Quotient.append(division)
-                except ZeroDivisionError:
-                    Quotient.append(0)
-                    print("division by 0")
-                except:
-                    Quotient.append(0)
-                    print("wrong type")
-            except IndexError:
-                Quotient.append(0)
-                print("out of range")
-    except:
-        pass
-    return Quotient
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except TypeError:
+            print("wrong type")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            Quotient.append(result)
+
+    return (Quotient)
