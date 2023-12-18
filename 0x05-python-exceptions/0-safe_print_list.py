e04b38e9e61a096fac36ce47bdd26a968f304be8
Meron Edea <edeameron@gmail.com>
2023-12-18 09:11:13 -0900

    adding files

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,9 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
         except:
-            break
+            pass
         else:
             count += 1
 
+    print()
     return (count)
--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import sys
 
 def safe_print_integer_err(value):
     try:
+        import sys
         print("{:d}".format(value))
         return True
     except Exception as e:
