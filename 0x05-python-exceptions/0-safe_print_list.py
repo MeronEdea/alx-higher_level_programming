8f66a3c1aad90e266227a25e23c5d4dc287dd3f7
Meron Edea <edeameron@gmail.com>
2023-12-18 08:39:12 -0900

    adding files

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        count = 0
         for item in my_list:
-            if count >= x:
-                break
-            print(item, end=" ")
-            count += 1
-        print()
-        return count
+            print(item, end="")
     except:
-        return count
+        pass
